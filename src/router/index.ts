f4b5efbbf896e6cf3f624b70c8239df53f296bd9
Canelle_adotevi <canelleadotevi31@gmail.com>
2023-11-07 23:59:05 +0100

    new

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,17 +10,8 @@ import SigninView from '@/views/SigninView.vue'
 import VerifyEmailView from '@/views/VerifyEmailView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [ 
-    {
-      path:'/',
-      name:'home',
-      component:HomeView
-    },
-    {
-      path: '/verify',
-      name:'verify',
-      component: VerifyEmailView
-    },
+  routes: [
+  
      {
       path:'/signin',
       name: 'signin',
@@ -56,8 +47,47 @@ const router = createRouter({
       path:'/expensefilter',
       name:'expensefilter',
       component:ExpenseFilterView
+    },
+    {
+      path:'/',
+      name:'home',
+      component:HomeView
+    },
+    {
+      path: '/verify',
+      name:'verify',
+      component: VerifyEmailView
     }
+    
+  
    ]
 })
  
+
+/* router.beforeEach((to, form, next) => {
+  const token = localStorage.getItem("access_token");
+  const loggedIn= localStorage.getItem("user");
+  if(to.path == "/signin" && token) {
+    return next("/")
+  }
+  
+  const publicPages= [
+    "/signin",
+    "/signup",
+    "/"
+    
+  ]
+  var isMatched = to.matched && publicPages.includes(to.matched[0].path);
+  console.log(isMatched);
+  console.log(to.matched[0].path);
+
+  const authRequired = !publicPages.includes(to.path) && !isMatched;
+
+  if(authRequired && !loggedIn) {
+    next("/signin");
+  }else{
+    return next();
+  }
+  
+}) */
 export default router
